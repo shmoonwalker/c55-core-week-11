65044ee861f75cb78d9418a41a21adc881993fe6
Shadi <sh.moonwalker7@gmail.com>
2026-03-24 19:19:09 +0100

    Implement Time class with methods for hours, minutes, seconds.

--- task-1/Time.js
+++ task-1/Time.js
@@ -1,3 +1,35 @@
 export class Time {
-  // Your code here
+  #secondsFromMidnight
+  
+  constructor(hours, minutes , seconds) {
+   
+    this.#secondsFromMidnight = hours * 3600 + minutes * 60 + seconds
+  }
+   getHours() { 
+    return Math.floor(this.#secondsFromMidnight / 3600)
+  }
+   getMinutes() {
+    return Math.floor((this.#secondsFromMidnight % 3600) / 60)
+  }
+   getSeconds() {
+    return this.#secondsFromMidnight % 60
+  }
+
+  addSeconds(seconds) {   
+   this.#secondsFromMidnight += seconds;
+   this.#secondsFromMidnight = ((this.#secondsFromMidnight % 86400) + 86400) % 86400;
+  }
+  addMinutes(minutes) {
+    this.addSeconds(minutes * 60)
+  } 
+  addHours(hours) {
+    this.addSeconds(hours * 3600)
+  }
+  toString() {
+    const hours = String(this.getHours()).padStart(2, '0');
+    const minutes = String(this.getMinutes()).padStart(2, '0');
+    const seconds = String(this.getSeconds()).padStart(2, '0');
+    return `${hours}:${minutes}:${seconds}`;
+  }     
 }
+
--- task-1/index.js
+++ task-1/index.js
@@ -2,3 +2,8 @@ import { Time } from './Time.js';
 
 const time = new Time(13, 37, 0);
 console.log(time.toString()); // Output: "13:37:00"
+console.log(time.getHours()); // Output: 13
+time.addMinutes(10);
+console.log(time.toString());
+time.addSeconds(8000);
+console.log(time.toString());
